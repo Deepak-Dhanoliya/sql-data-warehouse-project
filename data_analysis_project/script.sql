c0efa68511deacb6ab187ff8f9e000b846175e80
Deepak Dhanoliya <ddhakad910@gmail.com>
2025-04-03 16:12:57 +0530

    Update script.sql

--- data_analysis_project/script.sql
+++ data_analysis_project/script.sql
@@ -286,3 +286,109 @@ CREATE VIEW gold.report_customers AS
 	FROM customer_aggregation
 
 SELECT * FROM gold.report_customers
+
+/*
+=======================================================================
+PRODUCT REPORT
+=======================================================================
+Purpose:
+     - This report consoldates key product metrics and behaviors
+
+Highlights:
+	1. Gather essential fields such as product name , category, subcategory and cost.
+	2. Segments products by revenue to identify High-Performance , Mid-Range , or Low-Performers.
+	3. Aggregates products-level matrics:
+		- total orders
+		- total sales
+		- total quantity sold
+		- total customers (unique)
+		- lifespan (in months)
+	4. Calculates valuable KPIs:
+		- recency (month since last order)
+		- average order revenue (AOR)
+		- average monthly revenue
+======================================================================
+*/
+
+-- Creating a view of report for end users and dashboarding
+CREATE VIEW gold.report_products AS
+	WITH base_query AS
+	(	
+		-- 1) Base Query : Retrives core columns from tables
+		SELECT
+			p.product_key,
+			p.product_number,
+			p.product_name,
+			p.category,
+			p.subcategory,
+			p.cost,
+			s.customer_key,
+			s.order_number,
+			s.order_date,
+			s.quantity,
+			s.sales
+		FROM gold.fact_sales s
+		LEFT JOIN gold.dim_products p
+		ON s.product_key = p.product_key
+		WHERE s.order_date IS NOT NULL
+	),
+	product_aggregations AS 
+	(
+		-- 2) product aggregation: Summarize key metrix at product level
+
+		SELECT 
+			product_key,
+			product_number,
+			product_name,
+			category,
+			subcategory,
+			cost,
+			COUNT(order_number) AS total_orders,
+			SUM(sales) AS total_sales,
+			SUM(quantity) AS total_quantity,
+			COUNT(DISTINCT customer_key) AS total_customers,
+			DATEDIFF(MONTH , MIN(order_date), MAX(order_date)) AS lifespan,
+			MAX(order_date) AS last_order_date
+		FROM base_query
+		GROUP BY 
+			product_key,
+			product_number,
+			product_name,
+			category,
+			subcategory,
+			cost
+	)
+
+	-- 3) Final Report
+	SELECT 
+		product_key,
+		product_number,
+		product_name,
+		category,
+		subcategory,
+		cost,
+		total_orders,
+		total_sales,
+		CASE 
+			WHEN total_sales < 20000 THEN 'Low-Performance'
+			WHEN total_sales < 50000 THEN 'Mid-Range'
+			ELSE 'High-Performance'
+		END AS performance,
+		total_quantity,
+		total_customers,
+		lifespan,
+		last_order_date,
+		DATEDIFF(MONTH, last_order_date , GETDATE()) AS recency,
+		CASE
+			WHEN total_orders = 0 THEN total_sales
+			ELSE total_sales / total_orders 
+		END AS avg_order_revenue,
+		CASE
+			WHEN lifespan = 0 THEN total_sales
+			ELSE total_sales / lifespan 
+		END AS avg_monthly_revenue
+	
+	FROM product_aggregations
+
+SELECT * 
+FROM gold.report_products
